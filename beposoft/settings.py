6b7e8a05238371bdaa34898262b4e602a9a22b37
KASHYAPCT <kashyapchandran38@gamil.com>
2026-01-07 10:30:13 +0530

    db change

--- beposoft/settings.py
+++ beposoft/settings.py
@@ -114,23 +114,23 @@ WSGI_APPLICATION = 'beposoft.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'beposoft_db',
-#         'USER': 'beposoft',
-#         'PASSWORD': 'bepoindia123',
-#         'HOST': '97.74.89.160',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'beposoft_db',
+        'USER': 'beposoft',
+        'PASSWORD': 'bepoindia123',
+        'HOST': '97.74.89.160',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
